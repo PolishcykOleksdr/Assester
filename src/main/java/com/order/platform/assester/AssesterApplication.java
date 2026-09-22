9c1d72ce781619f7e7b6dae8877353faed1856fa
Polischyk_Oleksandr <sashap07072010@gmail.com>
2026-09-22 20:27:01 +0300

    Init commit

---
+++ src/main/java/com/order/platform/assester/AssesterApplication.java
@@ -0,0 +1,13 @@
+package com.order.platform.assester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssesterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssesterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/order/platform/assester/AssesterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.order.platform.assester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssesterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
